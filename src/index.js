064a1eaf2fd605d7a9951b378dc66d81cd1952ba
FatkIlnur <fatkhiln@gmail.com>
2024-09-08 13:20:59 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result = `${str[i]}${result}`;
+  }
+  return parseInt(result);
 }
+
